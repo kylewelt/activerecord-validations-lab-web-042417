1120a5ebecef23b80e34079a96e6d9b123c98e5d
kylewelt <kylewelt@umich.edu>
2017-05-22 15:46:00 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbaity
+
+  CLICKBAIT = ["Won't Believe", "Secret", "Top", "Guess"]
+
+  def clickbaity
+    if !(title.nil? || CLICKBAIT.any? { |word| title.include?(word) })
+      errors.add(:clickbait, "The title is not sufficiently clickbait-y")
+    end
+  end
 end
